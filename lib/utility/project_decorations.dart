39592012d30ed295587ce5ba516341ba685151fe
Doğancan Öztürk <DogancanOzturk@yaani.com>
2022-05-17 01:37:04 +0300

    Readability updates

---
+++ lib/utility/project_decorations.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProjectDecorations {
+// Paddings
+  static const EdgeInsets homepage3dotsPadding = EdgeInsets.only(right: 5);
+  static const EdgeInsets centerImgPadding = EdgeInsets.only(top: 180);
+  static const EdgeInsets loginButtonTopPadding = EdgeInsets.only(top: 100);
+  static const EdgeInsets createAccountTopPadding = EdgeInsets.only(top: 20);
+// Margins
+  static const EdgeInsets homepage3dotsMargin = EdgeInsets.only(right: 5);
+// Colors
+  static const Color homepage3dotsBorderColor = Color.fromRGBO(0, 0, 0, 0.122);
+  static const TextStyle loginButtonTextColor = TextStyle(color: Color.fromRGBO(0, 0, 0, 1));
+  static const Color createAccountButtonColor = Color(0xFF363339);
+  static const Color loginButtonColor = Color(0xFFFCFFDF);
+// Radii
+  static final BorderRadius actions3dotsRadius = BorderRadius.circular(100);
+  static final BorderRadius homepageButtonsRadius = BorderRadius.circular(100);
+// Sizes
+  static const Size homepageButtonsSize = Size(300, 50);
+}
